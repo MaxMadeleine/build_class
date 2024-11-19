630bfb2cd40c429052dd53124ce643c85fdc2d34
MaxMadeleine <Max.Madeleine.G@gmail.com>
2024-11-19 17:32:55 +0100

    song controller and model fully complited and added postman funcktions

--- controlers/songController.js
+++ controlers/songController.js
@@ -8,7 +8,7 @@ songController.get('/songs', async (req, res) => {
     console.log(data);
     res.status(200).send(data);
 });
-//kan ikke fange id eller [0-9]
+
 songController.get('/songs/:id', async (req, res) => {
     console.log(req.params.id);
    const data = await SongModel.getRecordById(req.params.id);
@@ -21,6 +21,18 @@ songController.post('/songs', async (req, res) => {
     const data = await SongModel.createRecord(req.body);
     console.log(data);
     res.status(201).send(data);
+});
+
+songController.put('/songs', async (req, res) => {
+    const data = await SongModel.updateRecord(req.body);
+    console.log(data);
+    res.status(200).send(data);
+});
+
+songController.delete('/songs', async (req, res) => {
+    const data = await SongModel.deleteRecord(req.body);
+    console.log(data);
+    res.status(200).send(data);
 });
 
 //single gør at du får den ud som en object istedet for en array
--- models/songModel.js
+++ models/songModel.js
@@ -51,7 +51,11 @@ getRecord = async (id) => {
       console.error(`Error: Cannot fetch songs, ${error}`);
     }
   };
-
+/**
+ * 
+ * @param {object} formdata 
+ * @returns data object
+ */
   static createRecord = async formdata => {
     try {
       const { data, error } = await supabase
@@ -70,5 +74,62 @@ getRecord = async (id) => {
       console.error(`Error: Cannot fetch songs, ${error}`);
     }
   };
-}
 
+  /**
+ * id record
+ * @param {object} formdata 
+ * @returns data object
+ */
+  static updateRecord = async formdata => {
+    try {
+      const { data, error } = await supabase
+      .from("songs")
+     .update([{
+      title: formdata.title,
+      content: formdata.content,
+      lyrics: formdata.lyrics,
+    }])
+    .eq("id", formdata.id)
+    .select()
+    .single()    
+
+      if (error) {
+        throw new Error(error.message);
+      }
+      return data;
+    } catch (error) {
+      console.error(`Error: Cannot fetch songs, ${error}`);
+    }
+  };
+
+
+  /**
+   * delete record
+   * @param {object} formdata 
+   * @returns 
+   */
+  static deleteRecord = async formdata => {
+   try {
+    const { data, error } = await supabase
+    .from("songs")
+    .delete()
+    .eq("id", formdata.id)
+
+    console.log(data
+    );
+    
+
+    if (error) {
+      throw error;
+    }
+    else{
+      return { message: "deleted file ok"};
+    }
+    return data;
+   }
+   catch (error) {
+    console.error(error);
+    
+  };
+};
+};
